26cecc5b5e0399d44a3f5e453672e92933ca2b03
David <dav_8612@live.com>
2021-04-18 11:09:37 -0400

    Added myapp folder for 355 Labs

---
+++ myapp/app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get('/', (request, response) =>{
+    response.send('Hello, World!');
+});
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
